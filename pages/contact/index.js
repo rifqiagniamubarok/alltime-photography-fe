9bfc802461ac72e8025f4750f68f59a2b85c5be6
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-11-27 08:09:16 +0700

    Feat: contact

---
+++ pages/contact/index.js
@@ -0,0 +1,8 @@
+import BasicLayout from '@/components/templates/BasicLayout';
+import React from 'react';
+
+const Contact = () => {
+  return <BasicLayout>Contact</BasicLayout>;
+};
+
+export default Contact;
