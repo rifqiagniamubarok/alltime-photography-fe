0e5805cf302f88244c593659f87535602aae4b2f
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-11-28 11:09:34 +0700

    Feat: add responsive package

--- components/templates/PackageLayout.js
+++ components/templates/PackageLayout.js
@@ -4,11 +4,11 @@ import BasicLayout from './BasicLayout';
 const PackageLayout = ({ children }) => {
   return (
     <BasicLayout>
-      <section className="mt-20 space-y-4 ">
+      <section className="mt-10 md:mt-20 space-y-2 md:space-y-4 ">
         <div className="font-passions">
           <p className="text-6xl text-center text-sage-dark">Package</p>
         </div>
-        <div className="bg-beige-sage  py-20 px-10">{children}</div>
+        <div className="bg-beige-sage py-5 md:py-14 lg:py-20 px-4 md:px-8 lg:px-10">{children}</div>
       </section>
     </BasicLayout>
   );
--- pages/package/index.js
+++ pages/package/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Package = () => {
   return (
     <PackageLayout>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
         {[...Array(8)].map((_, index) => (
           <CardPackage key={index} />
         ))}
