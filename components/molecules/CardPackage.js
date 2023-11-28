d301a158389a33098ab2c0029371ddc8e3b298db
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-11-28 07:40:31 +0700

    Feat: responsive home n cardpackage

--- components/molecules/CardPackage.js
+++ components/molecules/CardPackage.js
@@ -3,16 +3,16 @@ import React from 'react';
 
 const CardPackage = ({ withButton = true }) => {
   return (
-    <div className="w-full bg-beige p-3">
+    <div className="w-full bg-beige p-2 md:p-3">
       <div className="w-full flex">
-        <div className="w-5 aspect-square rounded-full bg-beige-sage border-2 border-sage"></div>
+        <div className="w-3 md:w-5 aspect-square rounded-full bg-beige-sage border-2 border-sage"></div>
       </div>
-      <div className="p-4 flex-col justify-between font-tenor">
+      <div className="p-2 lg:p-4 flex-col justify-between font-tenor">
         <div className="">
-          <p className="text-6xl text-center text-sage-dark font-passions">Premium</p>
-          <p className="text-center text-sage text-xl">Rp. 4,000,000</p>
+          <p className="text-4xl lg:text-6xl text-center text-sage-dark font-passions">Premium</p>
+          <p className="text-center text-sage text-base lg:text-xl">Rp. 4,000,000</p>
         </div>
-        <div className="p-8">
+        <div className="p-6 md:p-2 lg:p-8 text-sm lg:text-base">
           <ul class="list-disc">
             {[...Array(5)].map((_, index) => (
               <li key={index}>Lorem ipsum sage ol the best</li>
@@ -22,7 +22,7 @@ const CardPackage = ({ withButton = true }) => {
         {withButton && (
           <div className="flex justify-center">
             <Link href={`/package/coba`}>
-              <button className="bg-sage-soft text-white px-10 py-2 rounded-md hover:bg-opacity-90">Book</button>
+              <button className="bg-sage-soft text-white px-5 lg:px-10 py-1 lg:py-2 rounded-md hover:bg-opacity-90 text-sm lg:text-base">Book</button>
             </Link>
           </div>
         )}
--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,10 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   return (
     <BasicLayout>
-      <section className="mt-20 space-y-20 pb-40">
+      <section className="mt-0 md:mt-20 space-y-10 md:space-y-20 pb-40">
         <div className="font-tenor">
-          <p className="text-2xl text-center text-sage-dark">About us</p>
-          <p className="text-base text-center text-sage-soft mt-4">
+          <p className="text-lg md:text-2xl text-center text-sage-dark">About us</p>
+          <p className="text-sm md:text-base text-center text-sage-soft mt-4">
             {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
@@ -21,8 +21,8 @@ export default function Home() {
           </p>
         </div>
         <div className="font-tenor">
-          <p className="text-2xl text-center text-sage-dark">Why choose us</p>
-          <p className="text-base text-center text-sage-soft mt-4">
+          <p className="text-lg md:text-2xl text-center text-sage-dark">Why choose us</p>
+          <p className="text-sm md:text-base text-center text-sage-soft mt-4">
             {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
@@ -30,17 +30,17 @@ export default function Home() {
           </p>
         </div>
         <div className="">
-          <p className="text-2xl text-center text-sage-dark">Check your scheduel in our availibility</p>
+          <p className="text-lg md:text-2xl text-center text-sage-dark">Check your scheduel in our availibility</p>
           <div className="flex  justify-center mt-4">
             <div className="w-80">
               <DatepickerCustom className="text-beige" />
             </div>
           </div>
         </div>
-        <div className="bg-beige-sage  p-10 ">
-          <p className="text-2xl text-center text-sage-dark mb-10">Our best offer</p>
+        <div className="bg-beige-sage  p-4 lg:p-10 ">
+          <p className="text-lg md:text-2xl text-center text-sage-dark mb-10">Our best offer</p>
 
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-2 lg:gap-4">
             {[...Array(3)].map((_, index) => (
               <CardPackage key={index} />
             ))}
