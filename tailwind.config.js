6140f6b3eafa1ae1c850283ed11b810a6c678321
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-12-03 12:45:18 +0700

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,11 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/flowbite-react/**/*.js',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './node_modules/flowbite-react/**/*.js',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      fontFamily: {
-        sans: ['Tenor Sans', 'sans-serif'],
-      },
       colors: {
         primary: '#ECE3CE',
         'sage-dark': '#3A4D39',
@@ -26,8 +19,8 @@ module.exports = {
         beige: '#ECE3CE',
         'beige-sage': '#D0D4CA',
       },
-      darkMode: 'class',
     },
   },
-  plugins: [require('flowbite/plugin'), nextui()],
+  darkMode: 'class',
+  plugins: [nextui(), require('flowbite/plugin')],
 };
