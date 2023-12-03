531046f9b180f79369e598dde68c2497bbabaedc
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-12-03 12:46:27 +0700

    FEat: initiate pacakge

---
+++ pages/admin/package/index.js
@@ -0,0 +1,8 @@
+import AdminLayout from '@/components/templates/AdminLayout';
+import React from 'react';
+
+const Package = () => {
+  return <AdminLayout>Package</AdminLayout>;
+};
+
+export default Package;
