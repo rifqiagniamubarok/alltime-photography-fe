4d010b3b0b04b8ef158331046564b257994af327
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-12-03 12:45:52 +0700

    Fix: remove nextui

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,5 @@ import '@/styles/globals.css';
 import { NextUIProvider } from '@nextui-org/react';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
-  );
+  return <Component {...pageProps} />;
 }
