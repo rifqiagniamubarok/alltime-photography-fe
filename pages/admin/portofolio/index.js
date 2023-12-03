01faaa784b8e56ef84e3220fd90d676af0de8b95
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-12-03 12:46:38 +0700

    Feat: initiate portofolio n schedule

---
+++ pages/admin/portofolio/index.js
@@ -0,0 +1,8 @@
+import AdminLayout from '@/components/templates/AdminLayout';
+import React from 'react';
+
+const Portofolio = () => {
+  return <AdminLayout>Portofolio</AdminLayout>;
+};
+
+export default Portofolio;
---
+++ pages/admin/schedule/index.js
@@ -0,0 +1,8 @@
+import AdminLayout from '@/components/templates/AdminLayout';
+import React from 'react';
+
+const Schedule = () => {
+  return <AdminLayout>Schedule</AdminLayout>;
+};
+
+export default Schedule;
